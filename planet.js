e3a4e80a63f82572e3dae3436b000d97dd37b732
EliasPeeters <Elias.peeters@me.com>
2020-10-10 04:29:02 +0200

    Added project

---
+++ planet.js
@@ -0,0 +1,16 @@
+function planet() {
+
+
+    this.create = function(radiusMovement, radiusPlanet, speed, color, x, y) {
+        this.radiusMovement = radiusMovement;
+        this.radiusPlanet = radiusPlanet;
+        this.speed = speed;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    };
+
+    this.draw = function () {
+        
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(720, 400);
+    background(0);
+
+    var centerX;
+    var centerY;
+
+    fill(204);
+    triangle(18, 18, 18, 360, 81, 360);
+}
+
+function draw() {
+
+}
